55e943c38ea456fe4155458e5f6621a8d85fd7e6
Luke <lphyo@student.42bangkok.com>
2025-06-25 16:45:59 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 age_input = input("Please tell me your age: ")
 age = int(age_input)
-print(f"You are currently {age} years old.")
-print(f"In 10 years, you'll be {age + 10} years old.")
-print(f"In 20 years, you'll be {age + 20} years old.")
-print(f"In 30 years, you'll be {age + 30} years old.")
+
+print("You are currently", age, "years old.")
+
+for years in [10, 20, 30]:
+    print("In", years, "years, you'll be", age + years, "years old.")
