ad72ccb0fcc25987e4f0561d38f35c6adfb0ea44
Luke <lphyo@student.42bangkok.com>
2025-06-26 15:14:18 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    input_str = sys.argv[1]
+    result = ''
+    for char in input_str:
+        if char == 'z':
 
